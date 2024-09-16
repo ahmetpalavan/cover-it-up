eb45dd6dbc1b31255a4ea0506aa126701ee36221
Ahmet Palavan <ahmetpalavann@gmail.com>
2024-09-16 23:18:26 +0300

    feat: Added a footer component to the layout and integrated new icons, links, reviews section, and button variants in the page component.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
 import { Navbar } from '~/components/navbar';
+import Footer from '~/components/footer';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
