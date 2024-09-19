26249e816ef17d76739702a9797f765410deff3b
Ahmet Palavan <ahmetpalavann@gmail.com>
2024-09-19 19:16:56 +0300

    feat: Added new constants MATERIALS and FINISHES to option-validator.ts for product material and finish options, including pricing information from PRODUCT_PRICES.

--- validators/option-validator.ts
+++ validators/option-validator.ts
@@ -1,3 +1,5 @@
+import { PRODUCT_PRICES } from '~/config/product';
+
 export const COLORS = [
   { label: 'Black', value: 'black', tw: 'zinc-900' },
   {
@@ -34,6 +36,42 @@ export const MODELS = {
     {
       label: 'iPhone 15',
       value: 'iphone15',
+    },
+  ],
+} as const;
+
+export const MATERIALS = {
+  name: 'material',
+  options: [
+    {
+      label: 'Silicone',
+      value: 'silicone',
+      description: undefined,
+      price: PRODUCT_PRICES.material.silicone,
+    },
+    {
+      label: 'Soft Polycarbonate',
+      value: 'polycarbonate',
+      description: 'Scratch-resistant coating',
+      price: PRODUCT_PRICES.material.polycarbonate,
+    },
+  ],
+} as const;
+
+export const FINISHES = {
+  name: 'finish',
+  options: [
+    {
+      label: 'Smooth Finish',
+      value: 'smooth',
+      description: undefined,
+      price: PRODUCT_PRICES.finish.smooth,
+    },
+    {
+      label: 'Textured Finish',
+      value: 'textured',
+      description: 'Soft grippy texture',
+      price: PRODUCT_PRICES.finish.textured,
     },
   ],
 } as const;
